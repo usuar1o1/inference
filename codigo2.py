6e09e4707c37cc1e4d0c40d72b29672b6f372fe5
holas <holas@gmail.com>
2025-06-25 09:44:00 -0500

    graph python

---
+++ codigo2.py
@@ -0,0 +1,18 @@
+# HI this a new line for a git push 
+
+import matplotlib.pyplot as plt
+
+# Datos para graficar
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+# Crear la gráfica
+plt.plot(x, y)
+
+# Añadir título y etiquetas
+plt.title("Ejemplo de gráfica simple")
+plt.xlabel("Eje X")
+plt.ylabel("Eje Y")
+
+# Mostrar la gráfica
+plt.show()
